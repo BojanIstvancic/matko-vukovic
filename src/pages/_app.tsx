7579e315c6805d98039ac5e5295cdb9ef54f4f32
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-07 12:42:08 +0100

    add new pages, add styled components, define constants

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,44 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { createGlobalStyle } from "styled-components";
+import "../styles/globals.css";
+
+const GlobalStyle = createGlobalStyle`
+:root {
+  --white: #fff;
+  --black: #000;
+  --transparent: transparent;
+
+  --gray: #C4C4C4;
+  --dark-gray: #424242;
+
+  --light-blue: #f0f8ff;
+  --blue: #67BDFF;
+  --dark-blue: #1c9cfd;
+  --darker-blue: #0079cf;
+  --yellow: #ffe554;
+  --pink: #ce679a;
+}
+
+::selection {
+    background: var(--blue);
+    color: var(--white);
+}
+
+html {
+  font-family: 'Sora', sans-serif;
+  color: var(--dark-gray);
+}
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Head>
+        <title key="title">Matko Vukovic</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
