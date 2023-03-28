c09c682dd67e54610581e5a4aef56b24d0e878e7
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-28 15:07:09 +0200

    adjust styled components load and adjust 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,40 @@
-const FourOhFour: React.FC<{}> = () => {
+import Container from "@/components/Container";
+import Layout from "@/components/Layout";
+import Link from "next/link";
+import styled from "styled-components";
+
+const LinkContent = styled.h2`
+  display: inline;
+
+  margin-bottom: 30px;
+  padding: 10px;
+
+  border: 1px solid var(--primary);
+  border-radius: 5px;
+  transition: all 500ms ease;
+
+  &:hover {
+    cursor: pointer;
+    background: var(--primary);
+    color: var(--white);
+  }
+`;
+const Error: React.FC<{}> = () => {
   return (
-    <>
-      <h1>404 - Page Not Found</h1>
-    </>
+    <Layout
+      title={"Matko Vuković | stranica ne postoji"}
+      content={"description"}
+    >
+      <Container>
+        <>
+          <h1>Stranica nije pronađena</h1>
+          <Link href="/" passHref>
+            <LinkContent>Vrati se na početnu stranicu</LinkContent>
+          </Link>
+        </>
+      </Container>
+    </Layout>
   );
 };
 
-export default FourOhFour;
+export default Error;
