c3b2c5779aace15b1f83dd95607384de02337773
BojanIstvancic <bojan.istvancic@gmail.com>
2023-04-13 09:09:16 +0200

    add static form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 import { useFormik } from "formik";
-import { object, string, boolean, date, InferType } from "yup";
+import * as Yup from "yup";
 
 export const FormikBasic = () => {
   const { handleSubmit, errors, touched, getFieldProps } = useFormik({
@@ -11,29 +11,57 @@ export const FormikBasic = () => {
       gender: "",
       terms: false,
     },
-    validationSchema: object({
-      fullName: string().min(3, "Min. 3 characters").required("Required"),
-      email: string().email("It should be a valid email").required("Required"),
-      password: string().min(6, "Min. 6 characters").required("Required"),
-      terms: boolean().isTrue("You must accept the terms!"),
-      rol: string().required("Required"),
-      gender: string().required("Required"),
+    validationSchema: Yup.object({
+      fullName: Yup.string().min(3, "Min. 3 characters").required("Required"),
+      email: Yup.string()
+        .email("It should be a valid email")
+        .required("Required"),
+      password: Yup.string().min(6, "Min. 6 characters").required("Required"),
+      terms: Yup.boolean().isTrue("You must accept the terms!"),
+      rol: Yup.string().required("Required"),
+      gender: Yup.string().required("Required"),
     }),
     onSubmit: (values) => {
       console.log(values);
     },
   });
   return (
-    <form>
-      <input type="text" placeholder="Full name" />
-
-      <input type="email" placeholder="E-mail" />
-
-      <input type="password" placeholder="Password" />
-
+    <form noValidate onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Full name"
+        {...getFieldProps("fullName")}
+        className={`${touched.fullName && errors.fullName && "error_input"}`}
+      />
+      {touched.fullName && errors.fullName && (
+        <span className="error">{errors.fullName}</span>
+      )}
+      <br />
+      <input
+        type="email"
+        placeholder="E-mail"
+        {...getFieldProps("email")}
+        className={`${touched.email && errors.email && "error_input"}`}
+      />
+      {touched.email && errors.email && (
+        <span className="error">{errors.email}</span>
+      )}
+      <br />
+      <input
+        type="password"
+        placeholder="Password"
+        {...getFieldProps("password")}
+        className={`${touched.password && errors.password && "error_input"}`}
+      />
+      {touched.password && errors.password && (
+        <span className="error">{errors.password}</span>
+      )}
+      <br />
       <div>
         <label htmlFor="rol">Select an option:</label>
-        <select id="rol">
+        <br />
+
+        <select id="rol" {...getFieldProps("rol")}>
           <option value="">--- Select ---</option>
           <option value="admin">Admin</option>
           <option value="user">User</option>
@@ -43,22 +71,50 @@ export const FormikBasic = () => {
 
       <div className="radio-group">
         <b>Gender: </b>
+        <br />
         <label>
-          <input type="radio" /> Man
+          <input
+            type="radio"
+            {...getFieldProps("gender")}
+            checked={getFieldProps("gender").value === "man"}
+            value="man"
+          />
+          Man
         </label>
+        <br />
         <label>
-          <input type="radio" /> Woman
+          <input
+            type="radio"
+            {...getFieldProps("gender")}
+            checked={getFieldProps("gender").value === "women"}
+            value="women"
+          />
+          Woman
         </label>
+        <br />
         <label>
-          <input type="radio" /> Other
+          <input
+            type="radio"
+            {...getFieldProps("gender")}
+            checked={getFieldProps("gender").value === "other"}
+            value="other"
+          />
+          Other
         </label>
-      </div>
 
+        {touched.gender && errors.gender && (
+          <span className="error">{errors.gender}</span>
+        )}
+      </div>
+      {touched.rol && errors.rol && <span className="error">{errors.rol}</span>}
       <label>
         <input type="checkbox" {...getFieldProps("terms")} />
         Terms and Conditions
+        {touched.terms && errors.terms && (
+          <span className="error">{errors.terms}</span>
+        )}
       </label>
-
+      <br />
       <button type="submit">Submit</button>
     </form>
   );
