0b4c8c8d782689a8741a1e468e173bb533716d77
BojanIstvancic <bojan.istvancic@gmail.com>
2023-04-24 12:05:01 +0200

    add types and display data

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -8,3 +8,33 @@ export interface Post {
   updatedAt: string;
   __v: string;
 }
+
+
+export enum EmployeeRoles {
+  DIRECTOR = "director",
+  DEPUTY = "deputy",
+  PROFESSOR = "professor",
+  SECRETARY = "secretary",
+  JANITOR = "janitor",
+  PEDAGOGUE = "pedagogue",
+  PSYCHOLOGIST = "psychologist"
+}
+
+export interface Employee {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  role: EmployeeRoles.DIRECTOR | EmployeeRoles.DEPUTY | EmployeeRoles.PROFESSOR | EmployeeRoles.SECRETARY | EmployeeRoles.JANITOR | EmployeeRoles.PEDAGOGUE | EmployeeRoles.PSYCHOLOGIST,
+  image: string;
+  createdBy: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: string;
+}
+
+export enum Administration {
+  DIRECTOR = EmployeeRoles.DIRECTOR,
+  SECRETARY = EmployeeRoles.SECRETARY,
+}
+
+
--- src/pages/zaposleni.tsx
+++ src/pages/zaposleni.tsx
@@ -5,6 +5,10 @@ import Image from "next/image";
 import styled from "styled-components";
 
 import Portrait from "/public/images/portrait.png";
+import { GetServerSideProps } from "next";
+import { apiCall } from "@/api/axios";
+import { API_Method, API_URL } from "@/constants/api";
+import { Administration, Employee } from "@/constants/types";
 
 const StyledStaff = styled.section``;
 
@@ -34,47 +38,58 @@ const Item = styled.div`
 `;
 const ItemImage = styled.div`
   position: relative;
+  margin-bottom: 10px;
+
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  overflow: hidden;
 `;
 const ItemName = styled.p`
   font-weight: bold;
 `;
 const ItemRole = styled.p``;
 
-const Staff: React.FC<{}> = () => {
+export interface StaffProps {
+  employees: {
+    employees: Employee[];
+  };
+}
+
+const Staff: React.FC<StaffProps> = ({ employees }) => {
+  const staffMembers: Employee[] = employees.employees;
+
+  const administration = staffMembers.filter(
+    (member) =>
+      member.role.includes(Administration.DIRECTOR) ||
+      member.role.includes(Administration.SECRETARY)
+  );
+
   return (
     <Layout title={"Matko Vuković | Zaposleni"} content={"description"}>
       <Container>
         <StyledStaff>
           <h1>Zaposleni</h1>
-          <StaffBlock>
-            <StaffHeading>Uprava škole</StaffHeading>
-            <ItemContainer>
-              <Item>
-                <ItemImage>
-                  <Image
-                    src={Portrait}
-                    alt="velika-skola"
-                    width={640}
-                    height={640}
-                  />
-                </ItemImage>
-                <ItemName>Mirjana Stevanovic</ItemName>
-                <ItemRole>Direktor</ItemRole>
-              </Item>
-              <Item>
-                <ItemImage>
-                  <Image
-                    src={Portrait}
-                    alt="velika-skola"
-                    width={640}
-                    height={640}
-                  />
-                </ItemImage>
-                <ItemName>Josipa Roncevic</ItemName>
-                <ItemRole>Sekretar</ItemRole>
-              </Item>
-            </ItemContainer>
-          </StaffBlock>
+          {administration && (
+            <StaffBlock>
+              <StaffHeading>Uprava škole</StaffHeading>
+              <ItemContainer>
+                {administration.map((member) => (
+                  <Item key={member._id}>
+                    <ItemImage>
+                      <Image
+                        src={member.image || Portrait}
+                        alt={member.firstName}
+                        width={640}
+                        height={639}
+                      />
+                    </ItemImage>
+                    <ItemName>{`${member.firstName} ${member.lastName}`}</ItemName>
+                    <ItemRole>{member.role}</ItemRole>
+                  </Item>
+                ))}
+              </ItemContainer>
+            </StaffBlock>
+          )}
           <StaffBlock>
             <StaffHeading>Stručna služba</StaffHeading>
             <ItemContainer>
@@ -108,6 +123,18 @@ const Staff: React.FC<{}> = () => {
       </Container>
     </Layout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await apiCall(API_URL.EMPLOYEES, API_Method.GET);
+
+  const employees = response.data;
+
+  return {
+    props: {
+      employees,
+    },
+  };
 };
 
 export default Staff;
