50318f7f0db1a0b9bf1a15955ed79b46780f7aa3
BojanIstvancic <bojan.istvancic@gmail.com>
2023-04-27 11:29:42 +0200

    remove surplus code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { links } from './constants/links'
-import { getCookie } from './helpers/cookieStorage';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
