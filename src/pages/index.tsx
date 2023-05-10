9e42cdb6310a3a0ff3b8053a737870f96561ae0e
BojanIstvancic <bojan.istvancic@gmail.com>
2023-05-10 16:10:57 +0200

    adjust index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,16 @@ import { useEffect, useState } from "react";
 
 import Layout from "../components/Layout";
 import Container from "@/components/Container";
-
-import SwiperImage1 from "/public/images/pages/index/swiper-1.jpg";
-import SwiperImage2 from "/public/images/pages/index/swiper-2.jpg";
-import SwiperImage3 from "/public/images/pages/index/swiper-3.jpg";
+import Home from "@/components/presentation/Home";
 
 import { Post } from "../constants/types";
 import { getBlogPostItems } from "../api/blog";
-import Home from "@/components/presentation/Home";
 
 const HomeContainer: React.FC = ({}) => {
   const [blogPosts, setBlogPosts] = useState<Post[]>([]);
 
   useEffect(() => {
+    // put  this in redux later
     const fetchData = async () => {
       const response = await getBlogPostItems("limit=3");
 
