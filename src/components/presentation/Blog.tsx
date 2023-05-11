789df5b566196ca49334a25d9c72440ad3104d0c
BojanIstvancic <bojan.istvancic@gmail.com>
2023-05-11 08:37:24 +0200

    vesti adjust page

---
+++ src/components/presentation/Blog.tsx
@@ -0,0 +1,113 @@
+import ReactPaginate from "react-paginate";
+import BlogPost from "@/components/BlogPost";
+
+import styled from "styled-components";
+import { Post } from "../../constants/types";
+
+const StyledBlog = styled.div`
+  margin-bottom: 25px;
+`;
+const BlogPostContainer = styled.div`
+  margin-bottom: 25px;
+
+  @media (min-width: 1200px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
+`;
+
+const PaginationContainer = styled.div`
+  .pagination {
+    display: flex;
+
+    padding-left: 0;
+
+    list-style: none;
+  }
+
+  .page-link {
+    position: relative;
+    display: block;
+
+    padding: 0.375rem 0.75rem;
+
+    color: var(--primary);
+    text-decoration: none;
+    background-color: var(--white);
+    border: 1px solid var(--gray-250);
+    cursor: pointer;
+
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+    &:hover {
+      background-color: var(--gray-200);
+      border-color: var(--gray-250);
+      z-index: 2;
+    }
+  }
+
+  .page-item.active .page-link {
+    color: var(--white);
+    background-color: var(--primary);
+    border-color: var(--primary);
+    z-index: 3;
+  }
+
+  @media (max-width: 599px) {
+    .page-item {
+      &:first-child,
+      &:last-child {
+        display: none;
+      }
+    }
+  }
+`;
+
+export interface BlogProps {
+  blogPostsToRender: Post[];
+  handlePageClick: (event: any) => void;
+  pageCount: number;
+}
+
+const Blog: React.FC<BlogProps> = ({
+  blogPostsToRender,
+  handlePageClick,
+  pageCount,
+}) => {
+  return (
+    <StyledBlog>
+      <h1>Vesti</h1>
+      <BlogPostContainer>
+        {!!blogPostsToRender.length &&
+          blogPostsToRender.map((post) => (
+            <BlogPost key={post._id} post={post} />
+          ))}
+      </BlogPostContainer>
+      <PaginationContainer>
+        <ReactPaginate
+          nextLabel="Sledeća >"
+          previousLabel="< Prethodna"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={1}
+          pageCount={pageCount}
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
+        />
+      </PaginationContainer>
+    </StyledBlog>
+  );
+};
+
+export default Blog;
--- src/pages/vesti/index.tsx
+++ src/pages/vesti/index.tsx
@@ -1,84 +1,25 @@
-import Container from "@/components/Container";
-import Layout from "@/components/Layout";
-import ReactPaginate from "react-paginate";
-
-import styled from "styled-components";
 import { useEffect, useState } from "react";
-import { GetServerSideProps } from "next";
+
+import Layout from "@/components/Layout";
+import Container from "@/components/Container";
+
+import Blog from "@/components/presentation/Blog";
 import { Post } from "../../constants/types";
-import BlogPost from "@/components/BlogPost";
 import { getBlogPostItems } from "@/api/blog";
 
-const StyledBlog = styled.div`
-  margin-bottom: 25px;
-`;
+const BlogPostContainer: React.FC<{}> = ({}) => {
+  const [blogPosts, setBlogPosts] = useState<Post[]>([]);
 
-const BlogPostContainer = styled.div`
-  margin-bottom: 25px;
+  useEffect(() => {
+    // put this in redux later
+    const fetchData = async () => {
+      const response = await getBlogPostItems();
 
-  @media (min-width: 1200px) {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 20px;
-  }
-`;
+      setBlogPosts(response.data.posts);
+    };
 
-const PaginationContainer = styled.div`
-  .pagination {
-    display: flex;
-
-    padding-left: 0;
-
-    list-style: none;
-  }
-
-  .page-link {
-    position: relative;
-    display: block;
-
-    padding: 0.375rem 0.75rem;
-
-    color: var(--primary);
-    text-decoration: none;
-    background-color: var(--white);
-    border: 1px solid var(--gray-250);
-    cursor: pointer;
-
-    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-
-    &:hover {
-      background-color: var(--gray-200);
-      border-color: var(--gray-250);
-      z-index: 2;
-    }
-  }
-
-  .page-item.active .page-link {
-    color: var(--white);
-    background-color: var(--primary);
-    border-color: var(--primary);
-    z-index: 3;
-  }
-
-  @media (max-width: 599px) {
-    .page-item {
-      &:first-child,
-      &:last-child {
-        display: none;
-      }
-    }
-  }
-`;
-
-export interface BlogProps {
-  posts: {
-    posts: Post[];
-  };
-}
-
-const Blog: React.FC<BlogProps> = ({ posts }) => {
-  const blogPosts: Post[] = posts.posts;
+    fetchData();
+  }, []);
 
   const [itemOffset, setItemOffset] = useState(0);
   const itemsPerPage = 6;
@@ -94,55 +35,22 @@ const Blog: React.FC<BlogProps> = ({ posts }) => {
     setItemOffset(newOffset);
   };
 
+  const blogPostsToRender = blogPosts.slice(
+    itemOffset,
+    itemOffset + itemsPerPage
+  );
+
   return (
     <Layout title={"Matko Vuković | Vesti"} content={"description"}>
       <Container>
-        <StyledBlog>
-          <h1>Vesti</h1>
-          <BlogPostContainer>
-            {blogPosts
-              .slice(itemOffset, itemOffset + itemsPerPage)
-              .map((post) => (
-                <BlogPost key={post._id} post={post} />
-              ))}
-          </BlogPostContainer>
-          <PaginationContainer>
-            <ReactPaginate
-              nextLabel="Sledeća >"
-              previousLabel="< Prethodna"
-              onPageChange={handlePageClick}
-              pageRangeDisplayed={3}
-              marginPagesDisplayed={1}
-              pageCount={pageCount}
-              pageClassName="page-item"
-              pageLinkClassName="page-link"
-              previousClassName="page-item"
-              previousLinkClassName="page-link"
-              nextClassName="page-item"
-              nextLinkClassName="page-link"
-              breakLabel="..."
-              breakClassName="page-item"
-              breakLinkClassName="page-link"
-              containerClassName="pagination"
-              activeClassName="active"
-            />
-          </PaginationContainer>
-        </StyledBlog>
+        <Blog
+          blogPostsToRender={blogPostsToRender}
+          handlePageClick={handlePageClick}
+          pageCount={pageCount}
+        />
       </Container>
     </Layout>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await getBlogPostItems();
-
-  const posts = response.data;
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
-
-export default Blog;
+export default BlogPostContainer;
