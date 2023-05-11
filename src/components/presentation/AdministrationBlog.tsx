9bb35f3034711020f6e152628a9fc767bdfdbeb1
BojanIstvancic <bojan.istvancic@gmail.com>
2023-05-11 12:39:21 +0200

    create presentational component for blog

---
+++ src/components/presentation/AdministrationBlog.tsx
@@ -0,0 +1,176 @@
+import Button from "../../components/Button";
+import { Modal } from "@mui/material";
+import Form from "../../components/Form";
+import Loading from "@/components/Loading";
+import BlogPost from "@/components/BlogPost";
+
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleMinus,
+  faPen,
+  faCirclePlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { Post } from "@/constants/types";
+
+const StyledBlog = styled.div``;
+const ButtonContainer = styled.div`
+  display: flex;
+
+  margin-bottom: 30px;
+`;
+const BlogPostContainer = styled.div`
+  margin-bottom: 25px;
+
+  @media (min-width: 1200px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
+`;
+const BlogPostInnerWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+`;
+
+const BlogPostButtonContainer = styled.div`
+  margin-right: 10px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const BlogTextInput = styled.input`
+  margin-right: 25px;
+`;
+
+const FormContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  background-color: var(--white);
+
+  &:not(.delete) {
+    width: 50%;
+    min-width: 300px;
+  }
+
+  h3 span {
+    color: var(--primary);
+    font-weight: bold;
+  }
+`;
+
+export interface AdministrationBlogProps {
+  handleCreatePost: (values: Post) => Promise<void>;
+  handleEditPost: (values: Post) => Promise<void>;
+  handleDeletePost: () => Promise<void>;
+  handleOpenModal: (action: string, id?: string | null) => void;
+  handleCloseModal: () => void;
+  openModal: boolean;
+  currentAction: string;
+  currentPost: Post | undefined;
+  isLoading: boolean;
+  blogPostsToRender: Post[];
+  handleSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const AdministrationBlog: React.FC<AdministrationBlogProps> = ({
+  handleCreatePost,
+  handleEditPost,
+  handleDeletePost,
+  handleOpenModal,
+  handleCloseModal,
+  openModal,
+  currentAction,
+  currentPost,
+  isLoading,
+  blogPostsToRender,
+  handleSearch,
+}) => (
+  <StyledBlog>
+    <ButtonContainer>
+      <BlogTextInput placeholder="Pronađi post" onChange={handleSearch} />
+      <Button clickFunction={() => handleOpenModal("add")}>
+        <FontAwesomeIcon
+          icon={faCirclePlus}
+          style={{ fontSize: 20, color: "white" }}
+        />
+      </Button>
+    </ButtonContainer>
+    <BlogPostContainer>
+      {blogPostsToRender.map((post: Post) => (
+        <BlogPostInnerWrapper key={post._id}>
+          <BlogPostButtonContainer>
+            <Button
+              buttonType="delete"
+              clickFunction={() => handleOpenModal("delete", post._id)}
+            >
+              <FontAwesomeIcon
+                icon={faCircleMinus}
+                style={{ fontSize: 20, color: "white" }}
+              />
+            </Button>
+            <Button
+              buttonType="edit"
+              clickFunction={() => handleOpenModal("edit", post._id)}
+            >
+              <FontAwesomeIcon
+                icon={faPen}
+                style={{ fontSize: 20, color: "white" }}
+              />
+            </Button>
+          </BlogPostButtonContainer>
+          <BlogPost post={post} />
+        </BlogPostInnerWrapper>
+      ))}
+    </BlogPostContainer>
+
+    <Modal
+      open={openModal}
+      onClose={handleCloseModal}
+      aria-labelledby="parent-modal-title"
+      aria-describedby="parent-modal-description"
+    >
+      <FormContainer className={currentAction}>
+        {currentAction === "add" && (
+          <Form
+            formName="Napravi nov post"
+            formType="post"
+            buttonName="Napravi post"
+            handleSubmit={handleCreatePost}
+          />
+        )}
+        {currentAction === "edit" && (
+          <Form
+            formName="Doradi post"
+            formType="post"
+            buttonName="Sačuvaj izmene"
+            buttonType="edit"
+            customInitialValues={currentPost}
+            handleSubmit={handleEditPost}
+          />
+        )}
+
+        {currentAction === "delete" && (
+          <>
+            <h3>
+              Potvrdite brisanje posta <span>{currentPost?.title}</span>
+            </h3>
+            <Button buttonType="delete" clickFunction={handleDeletePost}>
+              Potvrdi
+            </Button>
+          </>
+        )}
+
+        {isLoading && <Loading />}
+      </FormContainer>
+    </Modal>
+  </StyledBlog>
+);
+
+export default AdministrationBlog;
--- src/pages/administracija/blog.tsx
+++ src/pages/administracija/blog.tsx
@@ -1,22 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Layout from "@/components/AdministrationLayout";
-import Button from "../../components/Button";
-import { Modal } from "@mui/material";
-import Form from "../../components/Form";
-import Loading from "@/components/Loading";
-import BlogPost from "@/components/BlogPost";
+import AdministrationBlog from "@/components/presentation/AdministrationBlog";
 
 import { Post } from "../../constants/types";
-import { GetServerSideProps } from "next";
 
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleMinus,
-  faPen,
-  faCirclePlus,
-} from "@fortawesome/free-solid-svg-icons";
 import {
   createBlogPostItem,
   deleteBlogPostItem,
@@ -24,73 +13,23 @@ import {
   getBlogPostItems,
 } from "@/api/blog";
 
-const StyledBlog = styled.div``;
-
-const ButtonContainer = styled.div`
-  display: flex;
-
-  margin-bottom: 30px;
-`;
-
-const BlogPostContainer = styled.div`
-  margin-bottom: 25px;
-
-  @media (min-width: 1200px) {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 20px;
-  }
-`;
-const BlogPostInnerWrapper = styled.div`
-  display: grid;
-  grid-template-columns: auto 1fr;
-`;
-
-const BlogPostButtonContainer = styled.div`
-  margin-right: 10px;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-`;
-
-const BlogTextInput = styled.input`
-  margin-right: 25px;
-`;
-
-const FormContainer = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 20px;
-  background-color: var(--white);
-
-  &:not(.delete) {
-    width: 50%;
-    min-width: 300px;
-  }
-
-  h3 span {
-    color: var(--primary);
-    font-weight: bold;
-  }
-`;
-
-export interface BlogProps {
-  posts: {
-    posts: Post[];
-  };
-}
-
-const Blog: React.FC<BlogProps> = ({ posts }) => {
-  const [blogPosts, setBlogPosts] = useState<Post[]>(posts.posts);
+const AdnministrationBlogContainer: React.FC = ({}) => {
+  const [blogPosts, setBlogPosts] = useState<Post[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [search, setSearch] = useState("");
   const [openModal, setOpenModal] = useState(false);
   const [currentPost, setCurrentPost] = useState<undefined | Post>(undefined);
   const [currentAction, setCurrentAction] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getBlogPostItems();
+
+      setBlogPosts(response.data.posts);
+    };
+
+    fetchData();
+  }, []);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
@@ -106,7 +45,7 @@ const Blog: React.FC<BlogProps> = ({ posts }) => {
 
   const handleCloseModal = () => setOpenModal(false);
 
-  const createPost = async (values: Post) => {
+  const handleCreatePost = async (values: Post) => {
     const data = {
       content: values.content,
       title: values.title,
@@ -130,7 +69,7 @@ const Blog: React.FC<BlogProps> = ({ posts }) => {
     setIsLoading(false);
   };
 
-  const editPost = async (values: Post) => {
+  const handleEditPost = async (values: Post) => {
     const data = {
       content: values.content,
       title: values.title,
@@ -177,107 +116,29 @@ const Blog: React.FC<BlogProps> = ({ posts }) => {
     setIsLoading(false);
   };
 
+  const blogPostsToRender = blogPosts.filter(
+    (post: Post) =>
+      post.content.toLowerCase().includes(search.toLowerCase()) ||
+      post.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <Layout title={"Matko Vuković | Blog"}>
-      <StyledBlog>
-        <ButtonContainer>
-          <BlogTextInput placeholder="Pronađi post" onChange={handleSearch} />
-          <Button clickFunction={() => handleOpenModal("add")}>
-            <FontAwesomeIcon
-              icon={faCirclePlus}
-              style={{ fontSize: 20, color: "white" }}
-            />
-          </Button>
-        </ButtonContainer>
-        <BlogPostContainer>
-          {blogPosts
-            .filter(
-              (post) =>
-                post.content.toLowerCase().includes(search.toLowerCase()) ||
-                post.title.toLowerCase().includes(search.toLowerCase())
-            )
-            .map((post) => (
-              <BlogPostInnerWrapper key={post._id}>
-                <BlogPostButtonContainer>
-                  <Button
-                    buttonType="delete"
-                    clickFunction={() => handleOpenModal("delete", post._id)}
-                  >
-                    <FontAwesomeIcon
-                      icon={faCircleMinus}
-                      style={{ fontSize: 20, color: "white" }}
-                    />
-                  </Button>
-                  <Button
-                    buttonType="edit"
-                    clickFunction={() => handleOpenModal("edit", post._id)}
-                  >
-                    <FontAwesomeIcon
-                      icon={faPen}
-                      style={{ fontSize: 20, color: "white" }}
-                    />
-                  </Button>
-                </BlogPostButtonContainer>
-                <BlogPost post={post} />
-              </BlogPostInnerWrapper>
-            ))}
-        </BlogPostContainer>
-
-        <Modal
-          open={openModal}
-          onClose={handleCloseModal}
-          aria-labelledby="parent-modal-title"
-          aria-describedby="parent-modal-description"
-        >
-          <FormContainer className={currentAction}>
-            {currentAction === "add" && (
-              <Form
-                formName="Napravi nov post"
-                formType="post"
-                buttonName="Napravi post"
-                handleSubmit={createPost}
-              />
-            )}
-            {currentAction === "edit" && (
-              <Form
-                formName="Doradi post"
-                formType="post"
-                buttonName="Sačuvaj izmene"
-                buttonType="edit"
-                customInitialValues={currentPost}
-                handleSubmit={editPost}
-              />
-            )}
-
-            {currentAction === "delete" && (
-              <>
-                <h3>
-                  Potvrdite brisanje posta <span>{currentPost?.title}</span>
-                </h3>
-                <Button buttonType="delete" clickFunction={handleDeletePost}>
-                  Potvrdi
-                </Button>
-              </>
-            )}
-
-            {isLoading && <Loading />}
-          </FormContainer>
-        </Modal>
-      </StyledBlog>
+      <AdministrationBlog
+        handleCreatePost={handleCreatePost}
+        handleEditPost={handleEditPost}
+        handleDeletePost={handleDeletePost}
+        handleOpenModal={handleOpenModal}
+        handleCloseModal={handleCloseModal}
+        openModal={openModal}
+        currentAction={currentAction}
+        currentPost={currentPost}
+        isLoading={isLoading}
+        blogPostsToRender={blogPostsToRender}
+        handleSearch={handleSearch}
+      />
     </Layout>
   );
 };
 
-export default Blog;
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await getBlogPostItems();
-
-  const posts = response.data;
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
+export default AdnministrationBlogContainer;
