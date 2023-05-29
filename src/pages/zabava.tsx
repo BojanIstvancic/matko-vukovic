b45b5ca9740ee5b0aada08aaa4adaf674927ef0c
BojanIstvancic <bojan.istvancic@gmail.com>
2023-05-29 23:33:42 +0200

    add some new words

--- src/pages/zabava.tsx
+++ src/pages/zabava.tsx
@@ -5,7 +5,143 @@ import Fun from "@/components/presentation/Fun";
 import { generateSolution } from "@/helpers/generateSolution";
 
 const FunContainer: React.FC<{}> = () => {
-  const solutions = ["domar", "basan", "vesti", "krilo", "avion"];
+  const solutions = [
+    "avion",
+    "anđeo",
+    "album",
+    "arome",
+
+    "basan",
+    "badem",
+    "baget",
+    "beton",
+
+    "celer",
+    "cigla",
+    "cimet",
+    "citat",
+
+    "čekić",
+    "čvrst",
+    "čopor",
+    "čorba",
+
+    "ćerka",
+    "ćilim",
+    "ćitap",
+    "ćorav",
+
+    "domar",
+    "dečko",
+    "deoba",
+    "devet",
+
+    "đuveč",
+    "đakon",
+    "đeram",
+    "đunta",
+
+    "efekt",
+    "ekipa",
+    "ekser",
+    "epika",
+
+    "farad",
+    "farba",
+    "farsa",
+    "fazan",
+
+    "grozd",
+    "gajba",
+    "galeb",
+
+    "harfa",
+    "hauba",
+    "heres",
+    "heres",
+    "hidra",
+
+    "iskra",
+    "ilios",
+    "ideja",
+    "ikona",
+
+    "junak",
+    "jarac",
+    "jasle",
+    "jetra",
+
+    "krilo",
+    "kanal",
+    "kanal",
+    "kobac",
+
+    "labud",
+    "lakat",
+    "lanac",
+    "lasta",
+
+    "majka",
+    "magla",
+    "majica",
+    "major",
+
+    "način",
+    "nakit",
+    "nalaz",
+    "nalog",
+
+    "osmeh",
+    "obim",
+    "oblak",
+    "obruč",
+
+    "pajac",
+    "patka",
+    "pekar",
+    "paket",
+
+    "rubin",
+    "rabin",
+    "radar",
+    "radio",
+
+    "sajam",
+    "sajla",
+    "santa",
+    "scena",
+
+    "šofer",
+    "šnala",
+    "špica",
+    "štala",
+
+    "tacna",
+    "tabor",
+    "tajga",
+    "taksi",
+
+    "ukras",
+    "ulica",
+    "uslov",
+    "usput",
+
+    "vesti",
+    "vijak",
+    "varoš",
+    "vapaj",
+
+    "zvono",
+    "zaliv",
+    "zanat",
+    "zapad",
+
+    "žarač",
+    "žvaka",
+    "žurka",
+    "ždral",
+  ];
+  // put this in backend and pull it from API
   const solution = generateSolution(solutions);
 
   return (
